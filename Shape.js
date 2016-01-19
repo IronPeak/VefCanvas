b8e4dbb824cfd7f9a157f5684a248105fcb8b6a4
helgir <helgir87@gmail.com>
2016-01-19 18:17:57 +0000

    Added a so it is possable to draw a line

--- Shape.js
+++ Shape.js
@@ -31,3 +31,55 @@ var Shape = Base.extend({
 
 	},
 });
+
+var Line = Shape.extend ({
+
+
+	constructor: function() {
+		this.base("Line");
+		
+
+	},
+
+	draw: function(canvas) {
+
+		//console.log(this.size.x, this.size.y);
+		canvas.strokeStyle = this.color;
+		canvas.beginPath();
+		canvas.moveTo(this.size.x, this.size.y);
+		canvas.lineTo(this.pos.x, this.pos.y);
+		canvas.stroke();
+		this.base(canvas);
+		
+		
+		
+	},
+
+	startDrawing:function(point) {
+		this.size.x = this.pos.x;
+		this.size.y = this.pos.y;
+	},
+
+
+	drawing:function(point) {
+		this.size.x = point.x;
+		this.size.y = point.y;
+
+
+	},
+
+	stopDrawing:function(point) {
+		this.size.x = point.x;
+		this.size.y = point.y;
+
+	},
+
+	added: function(canvas) {
+
+		
+	},	
+
+
+
+});
+
